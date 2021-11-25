418eb3a82b3d7953584b1a782faec3c89396169e
Leland <lelandcer@gmail.com>
2021-11-25 14:45:08 -0500

    Add simple delay to navigate to main view

--- app/src/main/java/com/lelandcer/twodo/features/splash/SplashFragment.kt
+++ app/src/main/java/com/lelandcer/twodo/features/splash/SplashFragment.kt
@@ -6,13 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.lelandcer.twodo.R
+import kotlinx.coroutines.delay
 
 
 /**
  * Splash screen fragment used to ease the user into the app on launch and mask any initial loading
  */
 class SplashFragment : Fragment() {
+
+    init {
+        lifecycleScope.launchWhenStarted {
+            // TODO Connect to any "ready" events if initial loading is needed
+            delay(2000)
+            val action = SplashFragmentDirections.actionSplashFragmentToTwoDoListsFragment()
+            findNavController().navigate(action)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
