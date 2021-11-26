7f3a75315696da719a7b0157fbfc79b9eab58484
Leland <lelandcer@gmail.com>
2021-11-25 20:54:06 -0500

    Add constructor defaults to Task

--- app/src/main/java/com/lelandcer/twodo/models/task/Task.kt
+++ app/src/main/java/com/lelandcer/twodo/models/task/Task.kt
@@ -1,14 +1,13 @@
 package com.lelandcer.twodo.models.task
 
 import com.lelandcer.twodo.models.id.Id
-import com.lelandcer.twodo.models.list.List
 import java.util.*
 
 class Task(
     var id: Id,
     var name: String,
-    var isCompleted: Boolean,
-    var completedAt: Date?
+    var isCompleted: Boolean = false,
+    var completedAt: Date? = null
 ) {
 
     override fun equals(other: Any?): Boolean =
