f6da2196014f9ffe4e510d8cfc1693b9ba98e401
Leland <lelandcer@gmail.com>
2021-11-25 20:59:59 -0500

    Add initial placeholder data

--- app/src/main/java/com/lelandcer/twodo/models/task/PlaceholderTaskRepository.kt
+++ app/src/main/java/com/lelandcer/twodo/models/task/PlaceholderTaskRepository.kt
@@ -1,6 +1,28 @@
 package com.lelandcer.twodo.models.task
 
+import com.lelandcer.twodo.models.id.IdFactory
+import com.lelandcer.twodo.models.id.UUIDFactory
+import com.lelandcer.twodo.models.list.List
+
 /** A static placeholder repository for lists
  * TODO remove once implemented */
 class PlaceholderTaskRepository : TaskRepository {
+    private val idFactory: IdFactory = UUIDFactory()
+    override fun indexFor(list: List): MutableCollection<Task> {
+        return list.tasks
+    }
+
+    override fun store(list: List, task: Task) {
+        list.tasks.add(task)
+    }
+
+    override fun create(list: List, name: String): Task {
+        val task = Task(idFactory.makeId(), name)
+        store(list, task)
+        return task
+    }
+
+    override fun delete(task: Task) {
+        TODO("Not yet implemented")
+    }
 }
