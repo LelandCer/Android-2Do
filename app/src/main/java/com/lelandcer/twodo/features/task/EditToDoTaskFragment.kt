f6368676f5b401e9bce26093b92554c58c4b5e22
Leland <lelandcer@gmail.com>
2021-11-28 11:59:41 -0500

    Bind ToDoTask to edit view

--- app/src/main/java/com/lelandcer/twodo/features/task/EditToDoTaskFragment.kt
+++ app/src/main/java/com/lelandcer/twodo/features/task/EditToDoTaskFragment.kt
@@ -1,14 +1,20 @@
 package com.lelandcer.twodo.features.task
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import com.lelandcer.twodo.R
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.lelandcer.twodo.databinding.FragmentEditToDoTaskBinding
+import com.lelandcer.twodo.main.ToDoViewModel
+import com.lelandcer.twodo.models.task.ToDoTask
 
-class EditToDoTaskFragment : DialogFragment() {
+class EditToDoTaskFragment : DialogFragment(), Observer<ToDoTask?> {
+
+    private lateinit var binding: FragmentEditToDoTaskBinding
+    private val toDoViewModel: ToDoViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -16,7 +22,17 @@ class EditToDoTaskFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_to_do_task, container, false)
+        binding = FragmentEditToDoTaskBinding.inflate(inflater, container, false)
+        toDoViewModel.currentToDoTask.observe(viewLifecycleOwner, this)
+        return binding.root
+    }
+
+    override fun onChanged(t: ToDoTask?) {
+        t?.let { bindModelToView(it) }
+    }
+
+    private fun bindModelToView(t: ToDoTask) {
+        binding.etTdtEditName.setText(t.name)
     }
 
 }
