eab7ad77b33f16ec1d6d80e9b87e9b01c6ac2753
Leland <lelandcer@gmail.com>
2021-11-28 09:52:08 -0500

    Fix titles, bind task data to view

--- app/src/main/java/com/lelandcer/twodo/features/task/ToDoTasksFragment.kt
+++ app/src/main/java/com/lelandcer/twodo/features/task/ToDoTasksFragment.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lelandcer.twodo.databinding.FragmentToDoTasksListBinding
+import com.lelandcer.twodo.features.list.ToDoListDisplay
+import com.lelandcer.twodo.main.ToDoViewModel
 import com.lelandcer.twodo.models.PlaceholderContent
+import com.lelandcer.twodo.models.list.ToDoList
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * A fragment representing a list of Items.
  */
-class ToDoTasksFragment : Fragment() {
+@AndroidEntryPoint
+class ToDoTasksFragment : Fragment(), Observer<ToDoList?> {
 
+    @Inject
+    lateinit var toDoListDisplay: ToDoListDisplay
     private lateinit var binding: FragmentToDoTasksListBinding
+    private val toDoViewModel: ToDoViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +40,30 @@ class ToDoTasksFragment : Fragment() {
             layoutManager = LinearLayoutManager(context)
             adapter = ToDoTaskRecyclerViewAdapter(PlaceholderContent.ITEMS)
         }
+
+        toDoViewModel.currentToDoList.observe(viewLifecycleOwner, this)
         return binding.root
     }
-    
+
+    override fun onChanged(toDoList: ToDoList?) {
+        toDoList?.let { bindToView(it) }
+    }
+
+    private fun bindToView(toDoList: ToDoList) {
+        val display = toDoListDisplay.forToDoList(toDoList)
+
+        val actionbar = (activity as AppCompatActivity?)?.supportActionBar
+        actionbar?.let {
+            //if we have an actionbar put the title there, otherwise use the in-fragment name textview
+            actionbar.title = display.name()
+            binding.tvTdtName.visibility = View.INVISIBLE
+
+        }
+
+        binding.tvTdtName.text = display.name()
+        binding.tvTdtCompletion.text = display.completionRatio()
+        binding.tvTdtDueAt.text = display.dueAt()
+        binding.tvTdtDueAtFormatted.text = display.dueAtDateFormat()
+    }
+
 }
