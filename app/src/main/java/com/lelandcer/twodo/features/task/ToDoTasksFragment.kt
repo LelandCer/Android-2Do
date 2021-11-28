a15fca096c10186d3f053ee1c320db1d6224a439
Leland <lelandcer@gmail.com>
2021-11-28 09:30:12 -0500

    Remove autogenerated junk from tasks fragment

--- app/src/main/java/com/lelandcer/twodo/features/task/ToDoTasksFragment.kt
+++ app/src/main/java/com/lelandcer/twodo/features/task/ToDoTasksFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.lelandcer.twodo.R
 import com.lelandcer.twodo.databinding.FragmentToDoTasksListBinding
 import com.lelandcer.twodo.models.PlaceholderContent
 
@@ -18,14 +15,10 @@ import com.lelandcer.twodo.models.PlaceholderContent
 class ToDoTasksFragment : Fragment() {
 
     private lateinit var binding: FragmentToDoTasksListBinding
-    private var columnCount = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
     }
 
     override fun onCreateView(
@@ -36,28 +29,10 @@ class ToDoTasksFragment : Fragment() {
 
         // Set the adapter
         with(binding.rvTdtTasks) {
-            layoutManager = when {
-                columnCount <= 1 -> LinearLayoutManager(context)
-                else -> GridLayoutManager(context, columnCount)
-            }
+            layoutManager = LinearLayoutManager(context)
             adapter = ToDoTaskRecyclerViewAdapter(PlaceholderContent.ITEMS)
         }
-
         return binding.root
     }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            ToDoTasksFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
-    }
+    
 }
