4dad5494c7fa14e723fb690c248a0dc8e65861a4
Leland <lelandcer@gmail.com>
2021-11-28 17:06:23 -0500

    update repositories to be more in line with future implementation

--- app/src/main/java/com/lelandcer/twodo/models/task/PlaceholderToDoTaskRepository.kt
+++ app/src/main/java/com/lelandcer/twodo/models/task/PlaceholderToDoTaskRepository.kt
@@ -1,20 +1,27 @@
 package com.lelandcer.twodo.models.task
 
+import android.util.Log
 import com.lelandcer.twodo.models.id.IdFactory
 import com.lelandcer.twodo.models.id.UUIDIdFactory
 import com.lelandcer.twodo.models.list.ToDoList
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 /** A static placeholder repository for lists
  * TODO remove once implemented */
-class PlaceholderToDoTaskRepository  @Inject constructor(): ToDoTaskRepository {
+class PlaceholderToDoTaskRepository @Inject constructor() : ToDoTaskRepository {
     private val idFactory: IdFactory = UUIDIdFactory()
     override fun indexFor(toDoList: ToDoList): MutableCollection<ToDoTask> {
-        return toDoList.toDoTasks
+        val id = toDoList.id.getKey()
+        val list = taskMap[id] ?: ArrayList()
+        taskMap[id] = list
+        return list
     }
 
     override fun store(toDoList: ToDoList, toDoTask: ToDoTask) {
-        toDoList.toDoTasks.add(toDoTask)
+        val tasks = indexFor(toDoList)
+        tasks.add(toDoTask)
     }
 
     override fun create(toDoList: ToDoList, name: String): ToDoTask {
@@ -22,6 +29,11 @@ class PlaceholderToDoTaskRepository  @Inject constructor(): ToDoTaskRepository {
     }
 
     override fun delete(toDoTask: ToDoTask) {
-        TODO("Not yet implemented")
+        val taskList = taskMap[toDoTask.listId.getKey()]
+        taskList?.remove(toDoTask)
+    }
+
+    companion object {
+        private val taskMap: MutableMap<String, MutableCollection<ToDoTask>> = HashMap()
     }
 }
