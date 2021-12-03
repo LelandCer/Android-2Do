63b721255cce96f4a59a25dc1def1203985bb9d3
Leland <lelandcer@gmail.com>
2021-12-03 17:43:10 -0500

    Turn the Id interface into an abstract class to use default equals and hashcode

--- app/src/main/java/com/lelandcer/twodo/database/models/StringId.kt
+++ app/src/main/java/com/lelandcer/twodo/database/models/StringId.kt
@@ -2,15 +2,8 @@ package com.lelandcer.twodo.database.models
 
 import com.lelandcer.twodo.models.id.Id
 
-class StringId(private val id: String) : Id {
+class StringId(private val id: String) : Id() {
     override fun getKey(): String {
         return id
-    }
-
-    override fun equals(other: Any?): Boolean =
-        other is Id && id== other.getKey()
-
-    override fun hashCode(): Int {
-        return id.hashCode()
     }
 }
--- app/src/main/java/com/lelandcer/twodo/models/id/Id.kt
+++ app/src/main/java/com/lelandcer/twodo/models/id/Id.kt
@@ -3,6 +3,13 @@ package com.lelandcer.twodo.models.id
 /**
  * Represents a unique identifier for a model
  */
-interface Id {
-    fun getKey(): String
+abstract class Id {
+    abstract fun getKey(): String
+
+    override fun equals(other: Any?): Boolean =
+        other is Id && getKey()== other.getKey()
+
+    override fun hashCode(): Int {
+        return getKey().hashCode()
+    }
 }
